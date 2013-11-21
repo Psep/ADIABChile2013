8e45ce14907f72bf97f2c41b6c976db9662dda7f
psep <psep@debian.cl>
2013-11-21 01:18:19 -0300

    add webapp

---
+++ webapp/js/webapp.js
@@ -0,0 +1,12 @@
+$('dl dd').hide();
+$('dl dt').click(function() {
+	if ($(this).hasClass('activo')) {
+		$(this).removeClass('activo');
+		$(this).next().slideUp();
+	} else {
+		$('dl dt').removeClass('activo');
+		$(this).addClass('activo');
+		$('dl dd').slideUp();
+		$(this).next().slideDown();
+	}
+}); 
